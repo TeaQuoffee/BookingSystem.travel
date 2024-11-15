685c07b020c49fabdaa0598eb40a990238a678ae
TeaQuoffee <jaytran2022@gmail.com>
2024-11-14 22:55:03 -0500

    created Passenger class

--- BookingSystem.java
+++ BookingSystem.java
@@ -1,21 +1,14 @@
 import java.util.Scanner;
-import java.util.Array;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.HashMap;
 
 public class BookingSystem {
     public static void main(String[] args) {
-        //Variables
-        Scanner scnr = new Scanner(System.in);
-        String aiports;
-        String names;
-        int age;
-               
-        //CODE
-        String test; //Delete later
-        test = scnr.next();
+        //variables
+            
 
-        System.out.print(test);
+        //code here
+    
     }
 }
