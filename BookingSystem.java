77a95dba7cc120c4916eb68d54ea39cd30e0cd7d
TeaQuoffee <jaytran2022@gmail.com>
2024-11-18 23:31:56 -0500

    Hashmap YIPPEE

--- BookingSystem.java
+++ BookingSystem.java
@@ -5,6 +5,7 @@ public class BookingSystem {
     @SuppressWarnings("resource")
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
+        Passenger customer = new Passenger();
         ArrayList<String> destinationList = new ArrayList<String>();
         int seatLimit = 151;
         int i = 1;
@@ -20,83 +21,98 @@ public class BookingSystem {
         destinationList.add("PARIS");
         Flight paris = new Flight("0003", "Paris, France", seatLimit);
         paris.setAvailableSeats(7);
-        System.out.println("Flying from Atlanta: what is your destination?");
-        System.out.println("______________\n\nNEW YORK\nTOKYO\nPARIS\n______________");
-        String destinationIn = scnr.nextLine();
-
-        while(i > 0) {
-            if (destinationIn.equals("NEW YORK")) {
-                if (newYork.getAvailableSeats() > 0) {
-                    System.out.println("Seat Available.");
-                    newYork.displayFlightDetails();
-                    i--;
-                } else {
-                    System.out.println("Seat Unavailable. Try a different destination!");
-                    destinationIn = scnr.nextLine();
-                }
-            } else if (destinationIn.equals("TOKYO")) {
-                if (tokyo.getAvailableSeats() > 0) {
-                    System.out.println("Seat Available.");
-                    tokyo.displayFlightDetails();
-                    i--;
-                } else {
-                    System.out.println("Seat Unavailable. Try a different destination!");      
-                    destinationIn = scnr.nextLine();        
-                }
-            } else if (destinationIn.equals("PARIS")) {
-                if (paris.getAvailableSeats() > 0) {
-                    System.out.println("Seat Available.");
-                    paris.displayFlightDetails();
-                    i--;
-                } else {
-                    System.out.println("Seat Unavailable. Try a different destination!");
-                    destinationIn = scnr.nextLine();
-                }
-            }
-        }    
-
         
-        // purchasing tickets    
-        System.out.println("Would you like to purchase a ticket? Y/N");
-        String purchaseAns = scnr.nextLine();
-
-        if (purchaseAns.equals("Y")) {
-            if (destinationIn.equals("NEW YORK")) {
-                System.out.println("Do you want to book a seat? Y/N");
-                String bookAns = scnr.nextLine();
-                if (bookAns.equals("Y")) {
-                    if (newYork.bookSeat()) {
-                        System.out.println("Seat booked successfully!");
-                    } else {
-                        System.out.println("Sorry, no seats are available for booking.");
-                    }
-                }
-                
-            }
-            else if (destinationIn.equals("TOKYO")) {
-                System.out.println("Do you want to book a seat? Y/N");
-                String bookAns = scnr.nextLine();
-                if (bookAns.equals("Y")) {
-                    if (tokyo.bookSeat()) {
-                        System.out.println("Seat booked successfully!");
-                    } else {
-                        System.out.println("Sorry, no seats are available for booking.");
-                    }
-                }
-            }
-            else if (destinationIn.equals("PARIS")) {
-                System.out.println("Do you want to book a seat? Y/N");
-                String bookAns = scnr.nextLine();
-                if (bookAns.equals("Y")) {
-                    if (paris.bookSeat()) {
-                        System.out.println("Seat booked successfully!");
-                    } else {
-                        System.out.println("Sorry, no seats are available for booking.");
-                    }
-                }
-            }
+        System.out.println("Welcome to TotallyNotDelta please enter your name: ");
+        String name = scnr.nextLine();
+        customer.addPass(name);
+        
+        if (customer.getPassengerId() != null) {
+            System.out.println("Welcome back " + name + "! What can we do for you today?");
+            System.out.println("1: [Book Flight]\n2: [Cancel Flight]");
         }
-        System.out.println("Thank you for using our system. Your flight from Atlanta to" + destinationIn + "Have a great day!");
-        
+
+        if (scnr.nextLine().equals("Book Flight")){
+            System.out.println("Welcome " + name +", Flying from Atlanta: what is your destination?");
+            System.out.println("______________\n\nNEW YORK\nTOKYO\nPARIS\n______________");
+            String destinationIn = scnr.nextLine();
+            while(i > 0) {
+                if (destinationIn.equals("NEW YORK")) {
+                    if (newYork.getAvailableSeats() > 0) {
+                        System.out.println("Seat Available.");
+                        newYork.displayFlightDetails();
+                        i--;
+                    } else {
+                        System.out.println("Seat Unavailable. Try a different destination!");
+                        destinationIn = scnr.nextLine();
+                    }
+                } else if (destinationIn.equals("TOKYO")) {
+                    if (tokyo.getAvailableSeats() > 0) {
+                        System.out.println("Seat Available.");
+                        tokyo.displayFlightDetails();
+                        i--;
+                    } else {
+                        System.out.println("Seat Unavailable. Try a different destination!");      
+                        destinationIn = scnr.nextLine();        
+                    }
+                } else if (destinationIn.equals("PARIS")) {
+                    if (paris.getAvailableSeats() > 0) {
+                        System.out.println("Seat Available.");
+                        paris.displayFlightDetails();
+                        i--;
+                    } else {
+                        System.out.println("Seat Unavailable. Try a different destination!");
+                        destinationIn = scnr.nextLine();
+                    }
+                }
+            }    
+    
+            
+            // purchasing tickets    
+            System.out.println("Would you like to purchase a ticket? Y/N");
+            String purchaseAns = scnr.nextLine();
+    
+            if (purchaseAns.equals("Y")) {
+                if (destinationIn.equals("NEW YORK")) {
+                    System.out.println("Do you want to book a seat? Y/N");
+                    String bookAns = scnr.nextLine();
+                    if (bookAns.equals("Y")) {
+                        if (newYork.bookSeat()) {
+                            System.out.println("Seat booked successfully!");
+                        } else {
+                            System.out.println("Sorry, no seats are available for booking.");
+                        }
+                    }
+                    
+                }
+                else if (destinationIn.equals("TOKYO")) {
+                    System.out.println("Do you want to book a seat? Y/N");
+                    String bookAns = scnr.nextLine();
+                    if (bookAns.equals("Y")) {
+                        if (tokyo.bookSeat()) {
+                            System.out.println("Seat booked successfully!");
+                        } else {
+                            System.out.println("Sorry, no seats are available for booking.");
+                        }
+                    }
+                }
+                else if (destinationIn.equals("PARIS")) {
+                    System.out.println("Do you want to book a seat? Y/N");
+                    String bookAns = scnr.nextLine();
+                    if (bookAns.equals("Y")) {
+                        if (paris.bookSeat()) {
+                            System.out.println("Seat booked successfully!");
+                        } else {
+                            System.out.println("Sorry, no seats are available for booking.");
+                        }
+                    }
+                }
+            }
+            System.out.println("Thank you for using our system. Your flight from Atlanta to" + destinationIn + "Have a great day!");
+    
+        }
+        else {
+            System.out.println("Sorry to hear that. Cancelling your flight.");
+            customer.removePass(name);
+        }
     }
 }
--- Passenger.java
+++ Passenger.java
@@ -1,10 +1,43 @@
+import java.util.HashMap;
+import java.util.Random;
+import java.util.HashSet;
+
 public class Passenger {
     // variables
     private String name;
     private boolean seatStatus;
     private boolean randomSeat;
-
+    private HashMap<String,String> passId = new HashMap<String, String>();
+     
     // mutators
+    public void setpassId(HashMap<String, String> passId) {
+        this.passId = passId;
+    }
+
+    public void populateManifest() {
+        passId.put("Jay Tran", "000001");
+        passId.put("Luke Liu", "000002");
+    }
+
+    public void addPass(String key) {
+        Random rand = new Random();
+        HashSet<Integer> usedDigits = new HashSet<>();
+        StringBuilder number = new StringBuilder();
+        
+        while (number.length() < 6) {
+            int digit = rand.nextInt(10);
+            if (!usedDigits.contains(digit)) {
+                usedDigits.add(digit);
+                number.append(digit);
+            }
+        }
+        passId.put(key, number.toString());
+    }
+
+    public void removePass(String key) {
+        passId.remove(key);
+    }
+
     public void setSeatStatus (int inSeat) {
         if (inSeat == 1) {
             seatStatus = true;
@@ -28,6 +61,10 @@ public class Passenger {
     }
 
     // getters
+    public HashMap<String, String> getPassengerId () {
+        return passId;
+    }
+    
     public boolean getSeatStatus(){
         return seatStatus;
     }
